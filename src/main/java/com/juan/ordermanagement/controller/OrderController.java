afd72cb02427a6bae6a9cbc069bd9d890731603e
Adrian Santos <87776401+ianteractive@users.noreply.github.com>
2026-09-25 13:03:38 +0800

    OrderController for createOrder

--- src/main/java/com/juan/ordermanagement/controller/OrderController.java
+++ src/main/java/com/juan/ordermanagement/controller/OrderController.java
@@ -1,5 +1,10 @@
 package com.juan.ordermanagement.controller;
 
+import com.juan.ordermanagement.dto.CreateOrderRequest;
+import com.juan.ordermanagement.dto.OrderResponse;
+import com.juan.ordermanagement.service.OrderService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/orders")
 public class OrderController {
 
+    private OrderService orderService;
 
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
+    @PostMapping
+    public OrderResponse createOrder(@RequestBody CreateOrderRequest createOrderRequest){
+        return orderService.createOrder(createOrderRequest);
+    }
 }
